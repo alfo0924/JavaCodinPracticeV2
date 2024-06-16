5e565e5aa46bd217dde64dab595c59a4db5184ff
user <d1204433@o365.fcu.edu.tw>
2024-06-16 21:56:27 +0800

    v1.1 commit n push

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,37 @@
+import java.util.*;
+import java.io.*;
+ class io
+{
+
+}
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
         System.out.println("Practice from Main");
+        try{
+            BufferedWriter br=new BufferedWriter(new FileWriter("IOTest.txt"));
+            Scanner scanner =new Scanner(System.in);
+            ArrayList<String>arl=new ArrayList<>();
+            String sc=scanner.nextLine();
+            while(!sc.equals("break"))
+            {
+            sc=scanner.nextLine();
+            arl.add(sc);
+            }
+            for(String str:arl)
+            {
+                br.write(str);
+                br.newLine();
+            }
+
+            br.close();
+        }
+        catch(IOException ioe){
+            System.out.println(ioe);
+        }
+
+
+
     }
 }
